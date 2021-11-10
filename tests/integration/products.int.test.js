ea86e1422855a62dcf08c953ca9ad80c7cfba8be
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-11-10 14:33:10 +0900

    feat: update get, update, delete products test code

--- tests/integration/products.int.test.js
+++ tests/integration/products.int.test.js
@@ -2,6 +2,7 @@ const request = require('supertest')
 const app = require('../../app_tdd')
 const newProduct = require('../../data/new-product.json')
 
+let firstProduct
 test("POST /products", async () => {
     const response = await request(app).post("/products").send(newProduct)
     expect(response.statusCode).toBe(201)
@@ -10,12 +11,56 @@ test("POST /products", async () => {
 })
 
 test("should return 500 on POST /products", async () => {
-    const response = await request(app)
-      .post("/products")
-      .send({ name: "description 제외하고 보내기" });
-  
+    const response = await request(app).post("/products").send({ name: "description 제외하고 보내기" })
     expect(response.statusCode).toBe(500);
     expect(response.body).toStrictEqual({
       message: "Product validation failed: description: Path `description` is required.",
     });
-  });
+});
+
+test('GET /products', async () => {
+  const response = await request(app).get('/products')
+  expect(response.statusCode).toBe(200)
+  expect(Array.isArray(response.body)).toBeTruthy()
+  expect(response.body[0].name).toBeDefined()
+  expect(response.body[0].description).toBeDefined()
+  firstProduct = response.body[0]
+})
+
+test('GET /product/:productId', async () => {
+  const response = await request(app).get('/products/' + firstProduct._id)
+  expect(response.statusCode).toBe(200)
+  expect(response.body.name).toBe(firstProduct.name)
+  expect(response.body.description).toBe(firstProduct.description)
+})
+
+test('GET id doesnt exist /products/:productId', async () => {
+  const response = await request(app).get('/products/618a682821b18afd0ef55777')
+  expect(response.statusCode).toBe(404)
+})
+
+test('PUT /products', async () => {
+  const res = await request(app).put('/products/' +  firstProduct._id).send({
+    name: 'updated name', description: 'updated description'
+  })
+  expect(res.statusCode).toBe(200)
+  expect(res.body.name).toBe('updated name')
+  expect(res.body.description).toBe('updated description')
+})
+
+test('should return 404 on PUT /products', async () => {
+  const res = await request(app).put('/products/' + "618a686e21b18afd0ef55744").send({
+    name: 'updated name', description: 'updated description'
+  })
+  expect(res.statusCode).toBe(404)
+})
+
+test('DELETE /products', async () => {
+  const res = await request(app).delete('/products/' + firstProduct._id).send()
+  expect(res.statusCode).toBe(200)
+})
+
+test('DELETE id doesnt exist /products/:productId', async () => {
+  const res = await request(app).delete('/products/' + "618a686e21b18afd0ef55744").send()
+  expect(res.statusCode).toBe(404)
+})
--- tests/unit/products.test.js
+++ tests/unit/products.test.js
@@ -2,8 +2,17 @@ const productController = require('../../controllers/products')
 const productModel = require('../../models/Product')
 const httpMocks = require('node-mocks-http')
 const newProduct = require('../../data/new-product.json')
+const allProducts = require('../../data/all-products.json')
 
 productModel.create = jest.fn()
+productModel.find = jest.fn()
+productModel.findById = jest.fn()
+productModel.findByIdAndUpdate = jest.fn()
+productModel.findByIdAndDelete = jest.fn()
+
+const productId = '35f3qwrf2f3qrffqfw'
+const updateProduct = { name: 'updated name', description: 'updated description' }
+
 let req, res, next
 beforeEach(() => {
     req = httpMocks.createRequest()
@@ -25,7 +34,7 @@ describe('Product Controller Create', () => {
     test('should return 201 response code', async () => {
         await productController.createProduct(req, res, next)
         expect(res.statusCode).toBe(201)
-        expect(res._isEndCalled()).toBeTruthy();
+        expect(res._isEndCalled()).toBeTruthy()
     })
     test('should return json body in response', async () => {
         productModel.create.mockReturnValue(newProduct)
@@ -38,5 +47,134 @@ describe('Product Controller Create', () => {
         productModel.create.mockReturnValue(rejectedPromise)
         await productController.createProduct(req, res, next)
         expect(next).toBeCalledWith(errMsg)
+    })
+})
+
+describe('Product Controller Get', () => {
+    test('should have a getProduct function', () => {
+        expect(typeof productController.getProduct).toBe('function')
+    })
+    test('should call ProductModel.find({})', async () => {
+        await productController.getProduct(req, res, next)
+        expect(productModel.find).toHaveBeenCalledWith({})
+    })
+    test('should return 200 response', async () => {
+        await productController.getProduct(req, res, next)
+        expect(res.statusCode).toBe(200)
+        expect(res._isEndCalled()).toBeTruthy()
+    })
+    test('should return json body in response', async () => {
+        productModel.find.mockReturnValue(allProducts)
+        await productController.getProduct(req, res, next)
+        expect(res._getJSONData()).toStrictEqual(allProducts)
+    })
+    test('should handle error', async () => {
+        const errMsg = { message: "Error finding product data" } 
+        const rejectPromise = Promise.reject(errMsg)
+         productModel.find.mockReturnValue(rejectPromise)
+         await productController.getProduct(req, res, next)
+         expect(next).toHaveBeenCalledWith(errMsg)
+    })
+})
+
+describe('Product Controller GetById', () => {
+    test('should have a getProductById function', () => {
+        expect(typeof productController.getProductById).toBe('function')
+    })
+    test('should call productModel.findById', async () => {
+        req.params.productId = productId
+        await productController.getProductById(req, res, next)
+        expect(productModel.findById).toBeCalledWith(productId)
+    })
+    test('should return json body and response code 200', async () => {
+        productModel.findById.mockReturnValue(newProduct)
+        await productController.getProductById(req, res, next)
+        expect(res.statusCode).toBe(200)
+        expect(res._getJSONData()).toStrictEqual(newProduct)
+        expect(res._isEndCalled()).toBeTruthy()
+    })
+    test('should return 404 when item doesnt exist', async () => {
+        productModel.findById.mockReturnValue(null)
+        await productController.getProductById(req, res, next)
+        expect(res.statusCode).toBe(404)
+        expect(res._isEndCalled()).toBeTruthy()
+    })
+    test('should handle error', async () => {
+        const errMsg = { message: 'Error' }
+        const rejectPromise = Promise.reject(errMsg)
+        productModel.findById.mockReturnValue(rejectPromise)
+        await productController.getProductById(req, res, next)
+        expect(next).toHaveBeenCalledWith(errMsg)
+    })
+})
+
+describe('Product Controller Update', () => {
+    test('should have an updateProduct function', () => {
+        expect(typeof productController.updateProduct).toBe('function')
+    })
+    test('should call productModel.findByIdAndUpdate', async () => {
+        req.params.productId = productId
+        req.body = updateProduct
+        await productController.updateProduct(req, res, next)
+        expect(productModel.findByIdAndUpdate).toHaveBeenCalledWith(
+            productId, { name: 'updated name', description: 'updated description' }, { new: true }
+        )
+    })
+    test('should return json body and response code 200', async () => {
+        req.params.productId = productId
+        req.body = updateProduct
+        productModel.findByIdAndUpdate.mockReturnValue(updateProduct)
+        await productController.updateProduct(req, res, next)
+        expect(res.statusCode).toBe(200)
+        expect(res._isEndCalled()).toBeTruthy()
+        expect(res._getJSONData()).toStrictEqual(updateProduct)
+    })
+    test('should handle 404 when item doenst exist', async () => {
+        productModel.findByIdAndUpdate.mockReturnValue(null)
+        await productController.updateProduct(req, res, next)
+        expect(res.statusCode).toBe(404)
+        expect(res._isEndCalled()).toBeTruthy()  
+    })
+    test('should handle errors', async () => {
+        const errMsg = { message: 'error' }
+        const rejectPromise = Promise.reject(errMsg)
+        productModel.findByIdAndUpdate.mockReturnValue(rejectPromise)
+        await productController.updateProduct(req, res, next)
+        expect(next).toHaveBeenCalledWith(errMsg)
+    })
+})
+
+describe('Product Controller Delete', () => {
+    test('should have a deleteProduct function', () => {
+        expect(typeof productController.deleteProduct).toBe('function')
+    })
+    test('should call ProductModel.findByIdAndDelete', async () => {
+        req.params.productId = productId
+        await productController.deleteProduct(req, res, next)
+        expect(productModel.findByIdAndDelete).toBeCalledWith(productId)
+    })
+    test('should return 200 response', async () => {
+        let deletedProduct = {
+            name: 'delete product',
+            description: 'it is deleted'
+        }
+        productModel.findByIdAndDelete.mockReturnValue(deletedProduct)
+        await productController.deleteProduct(req, res, next)
+        expect(res.statusCode).toBe(200)
+        expect(res._getJSONData()).toStrictEqual(deletedProduct)
+        expect(res._isEndCalled()).toBeTruthy()
+    })
+    test('should handle 404 when item doesnt exist', async () => {
+        productModel.findByIdAndDelete.mockReturnValue(null)
+        await productController.deleteProduct(req, res, next)
+        expect(res.statusCode).toBe(404)
+        expect(res._isEndCalled()).toBeTruthy()
+    })
+    test('should handle errors', async () => {
+        const errMsg = { message: 'error deleting' }
+        const rejectPromise = Promise.reject(errMsg)
+        productModel.findByIdAndDelete.mockReturnValue(rejectPromise)
+        await productController.deleteProduct(req, res, next)
+        expect(next).toHaveBeenCalledWith(errMsg)
     })
 })

5a56732de4ee7192901285274ff9810a8fc91299
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-11-10 14:32:42 +0900

    fix: update new middleware functions (get, update, delete products)

--- controllers/products.js
+++ controllers/products.js
@@ -9,6 +9,62 @@ const createProduct = async (req, res, next) => {
     }
 }
 
+const getProduct = async (req, res, next) => {
+    try {        
+        const allProducts = await productModel.find({})
+        res.status(200).json(allProducts)
+    } catch (err) {
+        next(err)
+    }
+}
+
+const getProductById = async (req, res, next) => {
+    try {
+        const product = await productModel.findById(req.params.productId)
+        if(product) {
+            res.status(200).json(product)   
+        } else {
+            res.status(404).send()
+        }
+    } catch (err) {
+        next(err)
+    }
+}
+
+const updateProduct = async (req, res, next) => {
+    try {
+        const updateProduct = await productModel.findByIdAndUpdate(
+            req.params.productId,
+            req.body,
+            { new: true }
+        )
+        if(updateProduct) {
+            res.status(200).json(updateProduct)
+        } else {
+            res.status(404).send()
+        }
+    } catch (err) {
+        next(err)
+    }
+}
+
+const deleteProduct = async (req, res, next) => {
+    try {
+        const deletedProduct = await productModel.findByIdAndDelete(req.params.productId)
+        if(deletedProduct) {
+            res.status(200).json(deletedProduct)
+        } else {
+            res.status(404).send()
+        }
+    } catch (err) {
+        next(err)
+    }
+}
+
 module.exports = {
-    createProduct
+    createProduct,
+    getProduct,
+    getProductById,
+    updateProduct,
+    deleteProduct
 }
--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { createProduct } = require('../controllers/products')
+const { createProduct, getProduct, getProductById, updateProduct, deleteProduct } = require('../controllers/products')
 
 router.post('/', createProduct)
-
+router.get('/', getProduct)
+router.get('/:productId', getProductById)
+router.put('/:productId', updateProduct)
+router.delete('/:productId', deleteProduct)
 module.exports = router
